515ab84d48a7bd4c52d2d76b989dabab9477c852
Rory Nugent <rory@prizepony.us>
2013-05-02 21:11:36 -0400

    Added check to see if the root user is running the scripts.

--- toggleAllGPIO.sh
+++ toggleAllGPIO.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# check if the script is run by root
+if [ "$(whoami)" != "root" ]; then
+	echo "You must run this script as a root user." 2>&1
+	exit 1
+fi
+
 # loop through all valid GPIO pin numbers
 for i in 0 1 4 7 8 9 10 11 14 15 17 18 21 22 23 24 25
 do
